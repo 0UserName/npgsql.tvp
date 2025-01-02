b89af90860c8e3d5f5d62bee7246707b41a2af49
0username <net.fedorax64@gmail.com>
2025-01-02 21:44:50 +0300

    Initial commit. Part 1

---
+++ Npgsql.Tvp/Internal/Convertors/DataTableConverter.cs
@@ -0,0 +1,59 @@
+﻿using Npgsql.Internal;
+using Npgsql.PostgresTypes;
+
+using Npgsql.Tvp.Internal.Packets;
+
+using System;
+using System.Data;
+
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Npgsql.Tvp.Internal.Convertors
+{
+    internal sealed class DataTableConverter<TTable> : PgStreamingConverter<TTable> where TTable : DataTable
+    {
+        private readonly PostgresCompositeType _type;
+
+        /// <inheritdoc/>
+        public override TTable Read(PgReader reader)
+        {
+            throw new NotImplementedException($"{ nameof(DataTable) } is not supported due to lack of need");
+        }
+
+        /// <inheritdoc/>
+        public override ValueTask<TTable> ReadAsync(PgReader reader, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException($"{ nameof(DataTable) } is not supported due to lack of need");
+        }
+
+        /// <inheritdoc/>
+        public override Size GetSize(SizeContext context, TTable value, ref object writeState)
+        {
+            DataTableArrayPacket packet = new
+            DataTableArrayPacket
+            (value, _type);
+
+            writeState = packet;
+
+            return packet.SizeTotal;
+        }
+
+        /// <inheritdoc/>
+        public override void Write(PgWriter writer, TTable value)
+        {
+            WriteAsync(writer, value).AsTask().Wait();
+        }
+
+        /// <inheritdoc/>
+        public override ValueTask WriteAsync(PgWriter writer, TTable value, CancellationToken cancellationToken = default)
+        {
+            return DataTableWriter.WriteAsync(writer, cancellationToken);
+        }
+
+        public DataTableConverter(PostgresCompositeType type)
+        {
+            _type = type;
+        }
+    }
+}
---
+++ Npgsql.Tvp/Internal/Packets/Contracts/IPacket.cs
@@ -0,0 +1,31 @@
+﻿namespace Npgsql.Tvp.Internal.Packets.Contracts
+{
+    internal interface IPacket
+    {
+        /// <summary>
+        /// Size of headers and payload.
+        /// </summary>
+        int SizeOverall
+        {
+            get;
+        }
+
+        /// <summary>
+        /// Size of headers.
+        /// </summary>
+        int SizeHeaders
+        {
+            get;
+        }
+
+        /// <summary>
+        /// Object identifiers (OIDs) are used internally by PostgreSQL as 
+        /// primary keys for various system tables. Type oid represents an 
+        /// object identifier.
+        /// </summary>
+        uint Oid
+        {
+            get;
+        }
+    }
+}
