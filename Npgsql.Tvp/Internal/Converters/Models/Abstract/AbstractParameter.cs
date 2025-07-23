63cdcbbf34a4ad15fceb901d64e15f86ee528caf
0username <net.fedorax64@gmail.com>
2025-07-23 16:28:17 +0300

    temp changes

---
+++ Npgsql.Tvp/Internal/Converters/Models/Abstract/AbstractParameter.cs
@@ -0,0 +1,122 @@
+﻿using Npgsql.Internal;
+using Npgsql.Internal.Postgres;
+
+using Npgsql.Tvp.Internal.Converters.Models.Contracts;
+
+using System;
+
+namespace Npgsql.Tvp.Internal.Converters.Models.Abstract
+{
+    internal abstract class AbstractParameter(string dataTypeName, int columnsCount, int rowsCount, PgSerializerOptions options) : IParameter
+    {
+        /// <inheritdoc/>
+        public int ColumnsCount
+        {
+            get => columnsCount;
+        }
+
+        /// <inheritdoc/>
+        public virtual int RowsCount
+        {
+            get => rowsCount;
+        }
+
+        /// <inheritdoc/>
+        public int MetadataSize
+        {
+            get => sizeof(int) + sizeof(int) + sizeof(uint) + Constants.DIMENSIONS * (sizeof(int) + sizeof(int)) + sizeof(int) * RowsCount;
+        }
+
+        /// <inheritdoc/>
+        public uint OID
+        {
+            get => options.GetArrayElementTypeId(new DataTypeName(dataTypeName).ToArrayName()).Oid.Value;
+        }
+
+        /// <inheritdoc/>
+        public abstract Value this[int row, int column]
+        {
+            get;
+        }
+
+        /// <summary>
+        /// Gets the offset 
+        /// to the start of 
+        /// the row.
+        /// </summary>
+        protected int GetY(int row)
+        {
+            return row * columnsCount;
+        }
+
+        /// <summary>
+        /// Gets 
+        /// the offset 
+        /// within the 
+        /// row.
+        /// </summary>
+        protected int GetYX(int row, int column)
+        {
+            return row * columnsCount + column;
+        }
+
+        /// <summary>
+        /// Gets the offset to 
+        /// the end of the row.
+        /// </summary>
+        protected int GetYX(int row)
+        {
+            return GetYX(row, columnsCount);
+        }
+
+        private static int GetItemSize(object item, PgConverter converter, Size writeRequirement)
+        {
+            object? writeState = default;
+
+            return item is DBNull or null ? -1 : Accessors.GetSizeOrDbNullAsObject(default, converter, DataFormat.Binary, writeRequirement, item, ref writeState).Value.Value;
+        }
+
+        protected Value CreateValue(object item, Type type)
+        {
+            PgTypeInfo pgTypeInfo = options.GetDefaultTypeInfo(type) ?? throw new NotSupportedException($"Type is not supported: {type.FullName}");
+
+            PgConverterResolution resolution = pgTypeInfo.GetObjectResolution(item); // {Npgsql.Internal.PgResolverTypeInfo} for DateTime
+
+            BufferRequirements bufferRequirements = pgTypeInfo.GetBufferRequirements(resolution.Converter, DataFormat.Binary) ?? throw new NotSupportedException($"Binary format is not supported: {pgTypeInfo.PgTypeId}");
+
+            return new Value(item, resolution.PgTypeId.Oid.Value, resolution.Converter, bufferRequirements.Write, GetItemSize(item, resolution.Converter, bufferRequirements.Write));
+        }
+
+        /// <inheritdoc/>
+        public int GetRowSize(int row)
+        {
+            int size = sizeof(int) + columnsCount * Value.METADATA_SIZE;
+
+            for (int i = GetY(row); i < GetYX(row); i++)
+            {
+                size += Math.Max(0, this[row, i % columnsCount].Size);
+            }
+
+            return size;
+        }
+
+        /// <inheritdoc/>
+        public int GetSize()
+        {
+            int size = MetadataSize;
+
+            for (int i = 0; i < RowsCount; i++)
+            {
+                size += GetRowSize(i);
+            }
+
+            return size;
+        }
+
+        /// <inheritdoc/>
+        public virtual void Dispose()
+        {
+            // NOP.
+        }
+    }
+}
---
+++ Npgsql.Tvp/Internal/Converters/ParameterWriter.cs
@@ -0,0 +1,71 @@
+﻿using Npgsql.Internal;
+
+using Npgsql.Tvp.Internal.Converters.Models;
+using Npgsql.Tvp.Internal.Converters.Models.Contracts;
+
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Npgsql.Tvp.Internal.Converters
+{
+    internal static class ParameterWriter
+    {
+        private static async ValueTask WriteValueAsync(PgWriter writer, Value value, CancellationToken cancellationToken)
+        {
+            writer.WriteUInt32(value.OID);
+            writer.WriteInt32(value.Size);
+
+            if (value.Size != -1)
+            {
+                // None of the converters
+                // use state, so we leave
+                // it empty.
+                //
+                // NOTE: Except CompositeConverter<T>.
+                using (await writer.BeginNestedWriteAsync(value.WriteRequirement, value.Size, default, cancellationToken).ConfigureAwait(default))
+                {
+                    await Accessors.WriteAsObjectAsync(value.Converter, writer, value.Item, cancellationToken).ConfigureAwait(default);
+                }
+            }
+        }
+
+        public static async ValueTask WriteAsync(PgWriter writer, CancellationToken cancellationToken)
+        {
+            using (IParameter parameter = (IParameter)writer.Current.WriteState)
+            {
+                if (writer.ShouldFlush(parameter.MetadataSize))
+                {
+                    await writer.FlushAsync(cancellationToken).ConfigureAwait(default);
+                }
+
+                writer.WriteInt32(Constants.DIMENSIONS);
+                writer.WriteInt32(Constants.FLAGS);
+                writer.WriteUInt32(parameter.OID);
+                writer.WriteInt32(parameter.RowsCount);
+                writer.WriteInt32(Constants.LOWER_BOUND);
+
+
+                for (int i = 0; i < parameter.RowsCount; i++)
+                {
+                    if (writer.ShouldFlush(sizeof(int) + sizeof(int)))
+                    {
+                        await writer.FlushAsync(cancellationToken).ConfigureAwait(default);
+                    }
+
+                    writer.WriteInt32(parameter.GetRowSize(i));
+                    writer.WriteInt32(parameter.ColumnsCount);
+
+                    for (int j = 0; j < parameter.ColumnsCount; j++)
+                    {
+                        if (writer.ShouldFlush(sizeof(uint) + sizeof(int)))
+                        {
+                            await writer.FlushAsync(cancellationToken).ConfigureAwait(default);
+                        }
+
+                        await WriteValueAsync(writer, parameter[i, j], cancellationToken);
+                    }
+                }
+            }
+        }
+    }
+}
